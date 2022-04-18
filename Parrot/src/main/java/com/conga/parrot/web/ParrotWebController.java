2e2943ce24ad9d61048f1b7d82164c2dce2245a9
Steven Burt <sburt@conga.com>
2022-04-18 10:22:48 -0400

    PAR-02: Document echo endpoint.

--- Parrot/src/main/java/com/conga/parrot/web/ParrotWebController.java
+++ Parrot/src/main/java/com/conga/parrot/web/ParrotWebController.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ParrotWebController {
+    /**
+     * Responds to the web request with the value RequestParameter.
+     *
+     * @param input the input to echo
+     * @return a 200 respond with the given input
+     */
     @GetMapping(value = "/")
     public ResponseEntity<String> echo(@RequestParam("value") String input) {
         return ResponseEntity.ok(input);
