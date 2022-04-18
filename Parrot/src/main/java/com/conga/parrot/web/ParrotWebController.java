b1fb526cbcd88f4b39deafe77e56fdea8d7a20c7
Steven Burt <sburt@conga.com>
2022-04-18 10:30:25 -0400

    PAR-02: Document Web Controller.

--- Parrot/src/main/java/com/conga/parrot/web/ParrotWebController.java
+++ Parrot/src/main/java/com/conga/parrot/web/ParrotWebController.java
@@ -4,6 +4,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * The main Parrot Web Controller.
+ */
 @RestController
 @RequestMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ParrotWebController {
